cacdeecfce6bfa015e734f8dc86fb65e98410eb8
Erica Nafziger <ericakait@Ericas-iMac.local>
2017-01-24 20:09:04 -0800

    add service to online store and dynamic routing

--- src/app/marketplace/marketplace.component.ts
+++ src/app/marketplace/marketplace.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Album } from '../album.model';
+import { AlbumService } from '../album.service';
 
 @Component({
   selector: 'app-marketplace',
   templateUrl: './marketplace.component.html',
-  styleUrls: ['./marketplace.component.css']
+  styleUrls: ['./marketplace.component.css'],
+  providers: [AlbumService]
 })
-export class MarketplaceComponent implements OnInit {
+export class MarketplaceComponent implements RTCDTMFToneChangeEventInit {
 
-  constructor() { }
+  constructor(private router: Router, private albumService: AlbumService){}
+
+  albums: Album[] = [];
 
   ngOnInit() {
+    this.albums = this.albumService.getAlbums();
   }
 
+ goToDetailPage(clickedAlbum: Album) {
+   this.router.navigate(['albums', clickedAlbum.id]);
+ }
 }
